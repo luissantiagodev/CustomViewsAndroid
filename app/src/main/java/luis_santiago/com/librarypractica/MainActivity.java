a3f416aed1603c8be3f79a00601ddbc4657fab4b
luissantiagodev <luissantiagodev@gmail.com>
2018-08-23 16:35:42 -0500

    added travis

--- app/src/main/java/luis_santiago/com/librarypractica/MainActivity.java
+++ app/src/main/java/luis_santiago/com/librarypractica/MainActivity.java
@@ -2,9 +2,11 @@ package luis_santiago.com.librarypractica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import luis.com.mylibrary.MyView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
